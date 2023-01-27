8a6d37f6224ab926b939fee7d50f04f7db5b29be
PaulbeckR <123000995+PaulbeckR@users.noreply.github.com>
2023-01-27 14:42:18 -0500

    Added procfile and Usersolutions.py file

---
+++ UserSolutions.py
@@ -0,0 +1,10 @@
+from GraphicsBuild import*
+
+
+def check_correct(new_game, complete_board):
+    for i in range(9):
+        for j in range(9):
+            if new_game[i][j] == complete_board[i][j]:
+                return True
+            
+            return "Game is not correct"    
