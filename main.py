59a9dfcae54d2af9cc3f8bea0be31e5b5c3b6575
PaulbeckR <123000995+PaulbeckR@users.noreply.github.com>
2023-01-27 23:25:49 -0500

    hide other packages

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
-import os
-import psycopg2
+#import os
+#import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#DATABASE_URL = os.environ['DATABASE_URL']
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 from BuildPuzzle import *
 from PreparePuzzle import*
