59aa7a276ab861288cee9f9ec174b18650061d7a
PaulbeckR <123000995+PaulbeckR@users.noreply.github.com>
2023-04-10 16:11:32 -0400

    errors

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const solveButton = document.getElementById("solve");
 const validateButton = document.getElementById("validate");
 
 // Add this function to fetch a Sudoku board from the backend
-async function fetchSudokuBoard() {
+async function fetchSudokuBoard(difficulty) { // Add the difficulty parameter
     try {
-      const response = await fetch("https://paulbeck.pythonanywhere.com/?difficulty=${difficulty}");
+      const response = await fetch(`https://paulbeck.pythonanywhere.com/api/sudoku?difficulty=${difficulty}`); // Use backticks for the string
       const board = await response.json();
       return board;
     } catch (error) {
       console.error("Error fetching the Sudoku board:", error);
       return null;
     }
-  }
-  
-  // Modify the newGame function to use the fetched board
-  async function newGame() {
+}
+
+// Modify the newGame function to use the fetched board
+async function newGame() {
     const difficultyForm = document.getElementById("difficulty-form");
     const selectedDifficulty  = difficultyForm.difficulty.value;
 
@@ -26,11 +26,10 @@ async function fetchSudokuBoard() {
     } else {
       alert("Error fetching new Sudoku board. Please try again.");
     }
-  }
-  
-  // Add an event listener to the "New Game" button
-  newGameButton.addEventListener("click", newGame);
-  
-  // Fetch and render an initial board when the page loads
-  newGame();
-  
+}
+
+// Add an event listener to the "New Game" button
+newGameButton.addEventListener("click", newGame);
+
+// Fetch and render an initial board when the page loads
+newGame();
